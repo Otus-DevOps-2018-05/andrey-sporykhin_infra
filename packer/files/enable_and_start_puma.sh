7d4290df01ab43795c1ebae35fbb091f752d40b0
Andrey Sporykhin <andrey.sporykhin@gmail.com>
2018-07-01 21:31:51 +0300

    fix paths

--- packer/files/enable_and_start_puma.sh
+++ packer/files/enable_and_start_puma.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-mv /home/gcp-user/reddit/puma.service /etc/systemd/system
+mv /tmp/puma.service /etc/systemd/system/
 systemctl enable puma.service
 systemctl start puma.service
