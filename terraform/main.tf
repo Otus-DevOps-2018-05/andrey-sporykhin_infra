da34adb9a81ac2c22da94e2d8b4129378769ce13
Andrey Sporykhin <andrey.sporykhin@gmail.com>
2018-07-09 01:20:19 +0300

    terraform initial

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,22 +1,56 @@
 provider "google" {
- version = "1.4.0"
- project = "otus-infra-207314"
- region = "europe-north1"
+  version = "1.4.0"
+  project = "${var.project}"
+  region  = "${var.region}"
 }
+
 resource "google_compute_instance" "app" {
-  name = "reddit-app"
+  name         = "reddit-app"
   machine_type = "g1-small"
-  zone = "europe-north1-b"
+  zone         = "${var.zone}"
+  tags         = ["reddit-app"]
+
   boot_disk {
     initialize_params {
-    image = "reddit-base"
-        }
+      image = "${var.disk_image}"
+    }
   }
- network_interface { 
-    network = "default"
-    access_config {}
- }
- metadata {
-   ssh-keys = "gcp-user:${file("~/.ssh/gcp-user.pub")}"
- }
-} 
+
+  network_interface {
+    network       = "default"
+    access_config = {}
+  }
+
+  metadata {
+    ssh-keys = "gcp-user:${file(var.public_key_path)}"
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "gcp-user"
+    agent       = false
+    private_key = "${file("${var.private_key_path}")}"
+  }
+
+  provisioner "file" {
+    source      = "files/puma.service"
+    destination = "/tmp/puma.service"
+  }
+
+  provisioner "remote-exec" {
+    script = "files/deploy.sh"
+  }
+}
+
+resource "google_compute_firewall" "firewall_puma" {
+  name    = "allow-puma-default"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["9292"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["reddit-app"]
+}
