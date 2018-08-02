86c060fa5791d5270cc81ad13fe170bfe672415e
Andrey Sporykhin <andrey.sporykhin@gmail.com>
2018-08-03 00:18:28 +0300

    fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -96,16 +96,3 @@ resource "google_compute_instance" "app2" {
     script = "files/deploy.sh"
   }
 }
-
-resource "google_compute_firewall" "firewall_puma" {
-  name    = "allow-puma-default"
-  network = "default"
-
-  allow {
-    protocol = "tcp"
-    ports    = ["9292"]
-  }
-
-  source_ranges = ["0.0.0.0/0"]
-  target_tags   = ["reddit-app2"]
-}
