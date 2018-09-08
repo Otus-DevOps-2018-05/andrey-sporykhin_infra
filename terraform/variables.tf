a4c781c3bb2881a8e3223d30824d56e7de42f90a
Andrey Sporykhin <andrey.sporykhin@gmail.com>
2018-09-08 21:48:24 +0300

    gcloud storage add

---
+++ terraform/variables.tf
@@ -0,0 +1,13 @@
+variable project {
+  description = "Project ID"
+}
+
+variable region {
+  description = "Region"
+  default     = "europe-north1"
+}
+
+variable zone {
+  description = "Zone"
+  default     = "europe-north1-b"
+}
