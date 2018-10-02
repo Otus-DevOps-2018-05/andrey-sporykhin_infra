9c1d0e4848791ab008b386d58034c4ddf989966f
Andrey Sporykhin <andrey.sporykhin@gmail.com>
2018-10-02 19:28:41 +0300

    git rebase trying

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -11,8 +11,6 @@ variable zone {
   description = "Zone"
   default     = "europe-north1-b"
 }
-<<<<<<< HEAD
-=======
 
 variable public_key_path {
   description = "Path to the public key used for ssh access"
@@ -47,4 +45,3 @@ variable db_disk_image {
   description = "Disk image for reddit db"
   default     = "reddit-db-base"
 }
->>>>>>> terraform-2 init commmit
