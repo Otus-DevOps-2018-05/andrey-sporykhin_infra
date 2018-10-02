1b8f2a0cfe451154c177eea491731019ae5fab30
Andrey Sporykhin <andrey.sporykhin@gmail.com>
2018-10-02 19:33:43 +0300

    merge

--- packer/files/enable_and_start_puma.sh
+++ packer/files/enable_and_start_puma.sh
@@ -3,7 +3,4 @@ set -e
 
 mv /tmp/puma.service /etc/systemd/system/
 systemctl enable puma.service
-<<<<<<< HEAD
-=======
 systemctl start puma.service
->>>>>>> 056356f22f2b79390722518b755c3699ab661fcb
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -11,7 +11,6 @@ variable zone {
   description = "Zone"
   default     = "europe-north1-b"
 }
-<<<<<<< HEAD
 
 variable public_key_path {
   description = "Path to the public key used for ssh access"
@@ -46,5 +45,3 @@ variable db_disk_image {
   description = "Disk image for reddit db"
   default     = "reddit-db-base"
 }
-=======
->>>>>>> 056356f22f2b79390722518b755c3699ab661fcb
